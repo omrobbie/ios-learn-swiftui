ddcb19187b9a050579d0d0d5b7c59eaf2b25d0b5
omrobbie <putu.roby@yahoo.com>
2020-08-27 07:09:24 +0800

    create get top headlines service

--- NewsApp/NewsApp/Network/NetworkManager.swift
+++ NewsApp/NewsApp/Network/NetworkManager.swift
@@ -20,4 +20,49 @@ class NetworkManager {
         }
         return value
     }()
+
+    // MARK: - The Url's
+    private let BASE_URL = "https://newsapi.org/v2"
+    private let TOP_HEADLINES = "/top-headlines"
+
+    // MARK: - Query parameters
+    private let PARAM_COUNTRY = "country"
+}
+
+// MARK: - Services
+extension NetworkManager {
+
+    func getTopHeadlines(completion: @escaping (Result<[News], Error>) -> ()) {
+        let urlString = NetworkManager.shared.BASE_URL + NetworkManager.shared.TOP_HEADLINES
+        guard var urlComponents = URLComponents(string: urlString) else {return}
+
+        urlComponents.queryItems = [
+            URLQueryItem(name: "apiKey", value: apiKey),
+            URLQueryItem(name: PARAM_COUNTRY, value: "us"),
+        ]
+
+        guard let url = urlComponents.url else {return}
+
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {return}
+
+            do {
+                let decodeData = try JSONDecoder().decode(NewsEnvelope.self, from: data)
+
+                DispatchQueue.main.async {
+                    completion(.success(decodeData.articles))
+                }
+            } catch {
+                print("Error", error.localizedDescription)
+                completion(.failure(error))
+                return
+            }
+        }
+    }
 }
