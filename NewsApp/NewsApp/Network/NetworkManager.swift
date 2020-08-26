4e80cc29a281394fe3a711ec273454b259227bc4
omrobbie <putu.roby@yahoo.com>
2020-08-26 20:21:13 +0800

    load ApiKey from Keys.plist

--- NewsApp/NewsApp/Network/NetworkManager.swift
+++ NewsApp/NewsApp/Network/NetworkManager.swift
@@ -11,4 +11,13 @@ import Foundation
 class NetworkManager {
 
     static let shared = NetworkManager()
+
+    private let apiKey: String = {
+        guard let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+            let dict = NSDictionary(contentsOfFile: path),
+            let value = dict["ApiKey"] as? String else {
+                return "ApiKey not found!"
+        }
+        return value
+    }()
 }
