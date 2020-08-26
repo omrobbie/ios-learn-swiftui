cd0844b9e760350244863a64d3231563b8b021d1
omrobbie <putu.roby@yahoo.com>
2020-08-26 15:28:43 +0800

    * start news app project *

---
+++ NewsApp/NewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by omrobbie on 26/08/20.
+//  Copyright © 2020 omrobbie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
