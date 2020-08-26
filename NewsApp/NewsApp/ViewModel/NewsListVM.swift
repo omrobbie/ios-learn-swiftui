50851d825588d4ff979232ac4f452459c68fc122
omrobbie <putu.roby@yahoo.com>
2020-08-27 07:51:02 +0800

    get news images

--- NewsApp/NewsApp/ViewModel/NewsListVM.swift
+++ NewsApp/NewsApp/ViewModel/NewsListVM.swift
@@ -19,11 +19,29 @@ class NewsListVM: ObservableObject {
             case .failure(let error): print(error.localizedDescription)
             case .success(let newsData):
                 let newsVM = newsData.map(NewsViewModel.init)
+                self.getImages(for: newsVM)
 
                 DispatchQueue.main.async {
                     self.news = newsVM
                 }
 
+            }
+        }
+    }
+
+    func getImages(for news: [NewsViewModel]) {
+        news.forEach {
+            if !$0.urlToImage.isEmpty {
+                let urlString = $0.urlToImage
+                NetworkManager.shared.downloadImage(urlString: urlString) { (result) in
+                    switch result {
+                    case .failure(let error): print(error.localizedDescription)
+                    case .success(let data):
+                        DispatchQueue.main.async {
+                            self.imageData[urlString] = data
+                        }
+                    }
+                }
             }
         }
     }
